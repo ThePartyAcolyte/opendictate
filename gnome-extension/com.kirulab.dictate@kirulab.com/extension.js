53c1b571a6c9975a2f4253035a35cd296c4a379e
Tomás D. López <butcherwutcher@outlook.com>
2026-07-28 12:17:15 -0400

    style: Increase icon_size to 18px and heighten gear button contrast in GNOME extension

--- gnome-extension/com.kirulab.dictate@kirulab.com/extension.js
+++ gnome-extension/com.kirulab.dictate@kirulab.com/extension.js
@@ -60,6 +60,7 @@ class OpenDictateIndicator extends PanelMenu.Button {
         // Gear Icon & Button for menu
         this._gearIcon = new St.Icon({
             icon_name: 'emblem-system-symbolic',
+            icon_size: 18,
             style_class: 'system-status-icon opendictate-gear-icon',
             x_align: Clutter.ActorAlign.CENTER,
             y_align: Clutter.ActorAlign.CENTER,
@@ -76,6 +77,7 @@ class OpenDictateIndicator extends PanelMenu.Button {
         // Microphone/Record Icon (Main Button)
         this._micIcon = new St.Icon({
             icon_name: 'audio-input-microphone-symbolic',
+            icon_size: 18,
             style_class: 'system-status-icon',
             x_align: Clutter.ActorAlign.CENTER,
             y_align: Clutter.ActorAlign.CENTER,
@@ -120,6 +122,7 @@ class OpenDictateIndicator extends PanelMenu.Button {
         this._sendButton = new St.Button({
             child: new St.Icon({
                 icon_name: 'mail-send-symbolic',
+                icon_size: 18,
                 style_class: 'system-status-icon opendictate-send-icon',
                 x_align: Clutter.ActorAlign.CENTER,
                 y_align: Clutter.ActorAlign.CENTER,
@@ -143,6 +146,7 @@ class OpenDictateIndicator extends PanelMenu.Button {
         this._cancelButton = new St.Button({
             child: new St.Icon({
                 icon_name: 'process-stop-symbolic',
+                icon_size: 18,
                 style_class: 'system-status-icon opendictate-cancel-icon',
                 x_align: Clutter.ActorAlign.CENTER,
                 y_align: Clutter.ActorAlign.CENTER,
